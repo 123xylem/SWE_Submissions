cc8d3a0298cd1fccf9e7c9ac58377702a09e8f15
Chris Cullen <cucullen111@gmail.com>
2024-08-20 08:44:34 +0000

    [LeetCode Sync] Runtime - 172 ms (48.79%), Memory - 18 MB (21.24%)

--- 0169-majority-element/solution.py
+++ 0169-majority-element/solution.py
@@ -1,12 +1,8 @@
-class Solution(object):
-    def majorityElement(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: int
-        """
-        half = len(nums)//2
-        tracker = {}
-        for i in range(len(nums)):
-            tracker[nums[i]] = tracker.get(nums[i], 0) +1
-            if tracker[nums[i]] > half:
-                return nums[i]
+class Solution:
+    def majorityElement(self, nums: List[int]) -> int:
+        count = defaultdict(list)
+        half = len(nums) / 2
+        for num in nums:
+            count[num] = count.get(num, 0) + 1
+            if count[num] >= half:
+                return num
