b774291db5db425f709caf8c8ea195d53e36d93b
Chris Cullen <cucullen111@gmail.com>
2024-09-05 13:32:52 +0000

    [LeetCode Sync] Runtime - 40 ms (28.86%), Memory - 16.3 MB (99.46%)

--- submissions/0066-plus-one/solution.py
+++ submissions/0066-plus-one/solution.py
@@ -3,20 +3,5 @@ class Solution:
         s = ''.join(str(i) for i in digits)
         s = int(s) + 1
         s = str(s)
-        # print(s, s.split(''))
         return [int(x) for x in s]
-        # if digits[-1] == 9:
-        #     digits.append(0)
-        # if digits[-2] == 9:
-        #     digits[-2] = 1
 
-        # for i, v in enumerate(digits[::-1]):
-        #     if v == 9:
-        #         digits[-1] = 1
-        #         digits.append(0)
-
-        #         digits[-1] = 1
-        #         digits.append(0)
-        # else:
-        #     digits[-1] += 1
-        return digits
