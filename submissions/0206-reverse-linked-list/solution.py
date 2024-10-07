4da37cee830f5915a512a2a574a200ed2547f2ab
Chris Cullen <cucullen111@gmail.com>
2024-10-07 15:03:36 +0000

    [LeetCode Sync] Runtime - 35 ms (71.81%), Memory - 17.7 MB (87.97%)

---
+++ submissions/0206-reverse-linked-list/solution.py
@@ -0,0 +1,24 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+
+class Solution:
+    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        # init 2 pointers
+        # copy the currents Next Node into nxt(temp variable) 
+        # Then we change currents nextNode to be the leftpointer
+        # we iterate left pointer to current node 
+        # We iterate value of curr to nxt (the next in list)
+        # We return Prev which is now the head pointer and will point to all the other nodes in reverse.
+        prev, curr = None, head;
+        while curr:
+            nxt = curr.next
+            curr.next = prev
+            prev = curr
+            curr = nxt
+    
+        return prev
+
+
