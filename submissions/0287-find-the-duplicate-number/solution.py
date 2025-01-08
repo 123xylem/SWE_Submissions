2dc2bdfe2eb4670543da7abe528078f26e638ee8
Chris Cullen <cucullen111@gmail.com>
2025-01-08 21:27:14 +0000

    [LeetCode Sync] Runtime - 23 ms (80.28%), Memory - 39.4 MB (5.82%)

--- submissions/0287-find-the-duplicate-number/solution.py
+++ submissions/0287-find-the-duplicate-number/solution.py
@@ -1,5 +1,8 @@
 class Solution:
     def findDuplicate(self, nums: List[int]) -> int:
+        #This uses O(n) memory
+        # Could use o(1) memory if I used floyd algo to find intersection with fast and slow
+        # Then from that slow pointer create new slow pointer and both iterate until they land on duplicate
         tracker = {None: None}
         for num in nums:
             if num not in tracker:
