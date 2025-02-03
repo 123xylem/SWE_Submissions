48bc6e4bb9834d72ee5f20574abffdbc84f7ecce
Chris Cullen <cucullen111@gmail.com>
2025-02-03 17:07:36 +0000

    [LeetCode Sync] Runtime - 3 ms (8.05%), Memory - 18.5 MB (81.09%)

--- submissions/0792-binary-search/solution.py
+++ submissions/0792-binary-search/solution.py
@@ -1,9 +1,11 @@
+# We can dramatically reduce search time with 2 pointers
+# get pointer mid point (l, r //2)
+# Move mid point l or right pending on it being lower or higher then target
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
         l, r = 0, len(nums) -1
         if nums[r] < target:
             return -1
-        
         while l <= r:
             mid_point = l + r // 2
             if nums[mid_point] == target:
@@ -13,3 +15,4 @@ class Solution:
             else:
                 r -= 1
         return -1
+
